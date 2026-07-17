9f5af936350c0dbf00dcb057833353fd949a8b68
Lucas Amaro <lucash.96@hotmail.com>
2026-07-17 00:55:02 -0300

    Chore: initial project setup

---
+++ src/test/java/br/com/library_management_api/LibraryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.library_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
